06b5ba8101252e5d2c74b4ab97067d32e5dfc38a
Dell Santos <dell@fullstacklabs.co>
2024-01-22 17:39:58 -0300

    deletando

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "dves-state"
-    key    = "teste"
-    region = "us-east-2"
-  }
-}
--- ec2.tf
+++
@@ -1,6 +0,0 @@
-
-resource "aws_instance" "amb-web" {
-  ami           = "ami-04f767d954fe2d2d1"
-  instance_type = "t2.micro"
-  key_name      = "prd"
-}
