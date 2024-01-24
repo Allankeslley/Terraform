ce27310e0056b1d45293b0c0e6be6ee9e7f9f70e
Dell Santos <dell@fullstacklabs.co>
2024-01-24 16:57:38 -0300

    comandos terraform para criaçao na aws

--- Autoscaling/aws_lb_listener.tf
+++ Autoscaling/aws_lb_listener.tf
@@ -8,3 +8,16 @@ resource "aws_lb_listener" "west" {
     target_group_arn = aws_lb_target_group.end.arn
   }
 }
+
+
+resource "aws_lb_listener" "wes" {
+  load_balancer_arn = aws_lb.west.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.dves.arn
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.end.arn
+  }
+}
