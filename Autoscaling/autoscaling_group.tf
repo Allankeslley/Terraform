1b37b8c3323a0d1e87a2d5d2f0e6126eec920369
Dell Santos <dell@fullstacklabs.co>
2024-01-24 15:55:48 -0300

    comandos terraform para criaçao na aws

--- Autoscaling/autoscaling_group.tf
+++ Autoscaling/autoscaling_group.tf
@@ -4,6 +4,7 @@ resource "aws_autoscaling_group" "glog" {
   desired_capacity   = 1
   max_size           = 1
   min_size           = 1
+  target_group_arns  = [aws_lb_target_group.end.arn]
   launch_template {
     id      = aws_launch_template.log.id
     version = aws_launch_template.log.latest_version
